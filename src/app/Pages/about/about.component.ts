9e561fca5faad2a97d5f5aaa64de28dfb75dfb2c
Ayman <aymanuddin1111.au@gmail.com>
2023-08-27 23:08:03 +0530

    home page added About page/footer

--- src/app/Pages/about/about.component.ts
+++ src/app/Pages/about/about.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MdbScrollspyModule, RouterModule],
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
+  exp = [
+    {
+      url: 'https://www.pangaeax.com/platform/login',
+      title: 'Pangaeax',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio et, culpa nisi aliquam minus libero, eius quidem amet',
+      tech: ['angular', 'TypeScript', 'Scss', 'BootStrap'],
+    },
+    {
+      url: 'https://samhita.org/',
+      title: 'Samhitha',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio et, culpa nisi aliquam minus libero, eius quidem amet',
+      tech: ['angular', 'TypeScript', 'Scss', 'BootStrap'],
+    },
+  ];
+  project = [
+    {
+      url: '/project/aot',
+      title: 'Attack On Titan',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio et, culpa nisi aliquam minus libero, eius quidem amet',
+      tech: ['angular', 'TypeScript', 'Scss', 'Bootstrap'],
+    },
+    {
+      url: '/project/aot',
+      title: 'Attack On Titan',
+      desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio et, culpa nisi aliquam minus libero, eius quidem amet',
+      tech: ['angular', 'TypeScript', 'Scss', 'Bootstrap'],
+    },
+  ];
 }
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from '../about/about.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, AboutComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
-constructor(){
-}
+  constructor() {}
+  scroll() {
+    const aboutLink = document.getElementById('about-link');
+    if (aboutLink) {
+      aboutLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        const aboutSection = document.getElementById('about');
+        if (aboutSection) {
+          aboutSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+  }
 }
