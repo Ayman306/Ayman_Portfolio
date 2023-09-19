326dfddbee7f156020953d0f59037a170c479877
Ayman <aymanuddin1111.au@gmail.com>
2023-09-19 22:37:48 +0530

    resume added

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from '../about/about.component';
 import { FooterComponent } from '../footer/footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,5 +13,18 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  constructor() {}
+  constructor(private route:Router) { }
+  navigateTo(route:string) {
+    const resumeUrl = '../../../assets/resume/Aymanuddin.pdf'; // Replace with the actual URL of your resume
+    // Create an anchor element to initiate the download
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+    link.target = '_blank'; // Open in a new tab (optional)
+    link.download = 'Aymanuddin resume.pdf';
+     link.click();
+    console.log(link)
+        this.route.navigate([`${route}`])
+
+  }
+
 }
